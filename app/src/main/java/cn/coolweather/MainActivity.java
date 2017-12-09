d3843382d12f65722dabe3dd1d801456ba84edfd
liuleiyky <liuleiyky@163.com>
2017-12-09 17:56:38 +0800

    完成欢迎界面

--- app/src/main/java/cn/coolweather/MainActivity.java
+++ app/src/main/java/cn/coolweather/MainActivity.java
@@ -2,12 +2,21 @@ package cn.coolweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvWelcom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initWidget();
+    }
+
+    private void initWidget() {
+        tvWelcom = (TextView) findViewById(R.id.tvWelcome);
     }
 }

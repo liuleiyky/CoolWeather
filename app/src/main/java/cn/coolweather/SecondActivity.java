8c3bd6318f999d8fda9a24bdf55162fe85df766d
liuleiyky <liuleiyky@163.com>
2017-12-09 17:58:52 +0800

    添加第二个界面

---
+++ app/src/main/java/cn/coolweather/SecondActivity.java
@@ -0,0 +1,17 @@
+package cn.coolweather;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by DELL on 2017/12/9.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
